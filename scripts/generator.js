69828b20500f694e22abd86b0f4dfea2660c6940
looklikeapro <jerryzhou00@gmail.com>
2015-10-02 15:51:31 -0400

    handle duplicate file names

--- scripts/generator.js
+++ scripts/generator.js
@@ -2,6 +2,9 @@ var fs = require('fs-extra')
 var getPageTree = require("./helpers/getPageTree");
 var getFilePath = require("./helpers/fileHelpers").getFilePath;
 var getFileName = require("./helpers/fileHelpers").getFileName;
+var getFileFolder = require("./helpers/fileHelpers").getFileFolder;
+var getFileExtension = require("./helpers/fileHelpers").getFileExtension;
+var saveUniqueFileSync = require("./helpers/fileHelpers").saveUniqueFileSync;
 
 fs.removeSync('./src');
 
@@ -10,16 +13,23 @@ var pageTemplate = fs.readFileSync("./scripts/templates/PageContainer.jsx").toSt
 getPageTree('./pages').then(function(files) {
 	fs.ensureDirSync("./src/containers");
 	walkPageTree(files, function(isFolder, key, filePath){
-		if (!isFolder) {
+		if (isFolder && filePath) {
 			var fileContent = fs.readFileSync(filePath).toString();
-			fs.writeFileSync("./src/containers/"+getFileName(filePath, true)+".jsx", pageTemplate.replace("@HTMLMARKUP", fileContent));
+			var componentContent = pageTemplate.replace("@HTMLMARKUP", fileContent);
+			saveUniqueFileSync("./src/containers/"+getFileFolder(filePath)+".jsx", componentContent);
+			
+		}
+		else if (!isFolder) {
+			var fileContent = fs.readFileSync(filePath).toString();
+			var componentContent = pageTemplate.replace("@HTMLMARKUP", fileContent);
+			saveUniqueFileSync("./src/containers/"+getFileName(filePath, true)+".jsx", componentContent);
 			// generatePath = filePath.replace(/\.\/pages/, "./src");
 			// fs.ensureDirSync(getFilePath(generatePath));
 			// fs.writeFileSync(generatePath, "Hey there!");
-
 		}
 	});
 });
+
 
 function walkPageTree(files, callback) {
 	for (var key in files) {
--- scripts/helpers/fileHelpers.js
+++ scripts/helpers/fileHelpers.js
@@ -1,3 +1,5 @@
+var fs = require('fs-extra');
+
 var exports = {
 	isUnixHiddenPath: function (path) {
 		return (/(^|\/)\.[^\/\.]/g).test(path);
@@ -16,6 +18,40 @@ var exports = {
 			}
 		}
 		return filePath.substr(filePath.lastIndexOf("/"));
+	},
+	getFileExtension: function (filePath) {
+		var slashIndex = filePath.lastIndexOf("/");
+		var dotIndex = filePath.lastIndexOf(".");
+		if (slashIndex < dotIndex) {
+			return filePath.substr(dotIndex);
+		}
+		else return "";
+	},
+	getFileFolder: function (filePath) {
+		filePath = filePath.substr(0, filePath.lastIndexOf("/"));
+		filePath = filePath.substr(filePath.lastIndexOf("/")+1);
+		return filePath;
+	},
+	saveUniqueFileSync: function (filePath, content) {
+		var saveFilePath = filePath;
+		while (exports.fileExistsSync(saveFilePath)) {
+			if (saveFilePath === filePath) {
+				saveFilePath = exports.getFileFolder(saveFilePath)+exports.getFileName(saveFilePath, true)+""+1+exports.getFileExtension(saveFilePath);
+			}
+			else {
+				saveFilePath = exports.getFileFolder(saveFilePath)+exports.getFileName(saveFilePath, true)+1+exports.getFileExtension(saveFilePath);
+			}
+		}
+		fs.writeFileSync(saveFilePath, content);
+		return saveFilePath;
+	},
+	fileExistsSync: function (filePath) {
+		try {
+			var fileCheck = fs.statSync(filePath);
+			return true;
+		} catch (e) {
+			return false;
+		}
 	}
 };
 
